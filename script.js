66fa145557e01385a3ef02f96771bc244971491b
Nado1717 <nassivetconan17@gmail.com>
2026-09-25 12:11:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+    });
+
+});
